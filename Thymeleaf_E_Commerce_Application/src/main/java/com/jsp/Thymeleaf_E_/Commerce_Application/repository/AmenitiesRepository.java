8b042351b51062fa5e798df56a450866974b0f93
vikasargade <130592654+vikasargade@users.noreply.github.com>
2024-07-08 23:48:26 +0530

    Delete Thymeleaf_E_Commerce_Application directory

--- Thymeleaf_E_Commerce_Application/src/main/java/com/jsp/Thymeleaf_E_/Commerce_Application/repository/AmenitiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.Thymeleaf_E_.Commerce_Application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.Thymeleaf_E_.Commerce_Application.entity.Amenities;
-
-public interface AmenitiesRepository  extends JpaRepository<Amenities, Integer>{
-
-}
